d8b2ef8c7997d3207e31481b9392bfb214efa2f3
williamaerus <113334672+williamaerus@users.noreply.github.com>
2022-09-16 19:33:08 +0200

    Create password.py

---
+++ password.py
@@ -0,0 +1,22 @@
+import hashlib
+attempts = [] # a list of all the wrong attempts
+def start():
+    pswd = input('password: ')
+    hashed_string = hashlib.sha256(pswd.encode('utf-8')).hexdigest() #creates a hash of the input (pswd)
+    hashed_password = '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8' #this is the hash of the password
+    if hashed_string == hashed_password: #if the hash of the input is equal to the hash of the password then print correct password
+        print("correct password")
+        #here you can add any command you want(remeber the indentation)
+
+
+    else:
+        attempts.append(pswd) #appends every wrong password to the wrong passwords list
+        nmbs = len(attempts) #measures how long the wrong password list is
+        if nmbs <= 3: #if the list is shorter or equal at 3 then ask the password again
+            print('incorrect password try again')
+            print(4 - nmbs,'attempts left')
+            start()
+        elif nmbs > 3: #if the list is longer then 3 then exit
+            #to personalize delete exit() and add any command you want (remeber the indentation)
+            exit()
+start()
